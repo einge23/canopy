2c98d94b31b719379ad1b549124665f19bcd51b4
einge23 <einge823@gmail.com>
2025-02-09 15:53:19 -0500

    initial commit

---
+++ components/pages/home/hour_card.tsx
@@ -0,0 +1,38 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "~/components/ui/card";
+import { Text } from "~/components/ui/text";
+import { Button } from "~/components/ui/button";
+
+interface HourCardProps {
+    hour: number;
+}
+
+export default function HourCard({ hour }: HourCardProps) {
+    const formattedHour = new Date(2024, 0, 1, hour).toLocaleString("en-US", {
+        hour: "numeric",
+        hour12: true,
+    });
+
+    return (
+        <Card className="w-full">
+            <CardHeader>
+                <CardTitle>{formattedHour}</CardTitle>
+                <CardDescription>Schedule for this hour</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Text>Activities planned for {formattedHour}</Text>
+            </CardContent>
+            <CardFooter>
+                <Button>
+                    <Text>Add Event</Text>
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+}
